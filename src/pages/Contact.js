2a26fbc823724098ae9e2822aa772f640680e657
Natalija Nikic <nnikiccc123@gmail.com>
2024-01-09 00:14:47 +0100

    Login stranica

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,33 @@
+import React from "react";
+
 const Contact = () => {
-    return <h1>Contact Me</h1>;
+  return (
+    <div className="paragraph-container">
+      <h1 className="home-title">Contact Us</h1>
+      <p>
+        Welcome to our application! We're excited to hear from you. Whether you
+        have questions about our services, need assistance, or just want to say
+        hello, we're here to help.
+      </p>
+      <p>
+        Email: contact@example.com <br />
+        Phone: +1234567890 <br />
+        Address: 123 Example Street, City, Country
+      </p>
+      <h1 >About Our Application</h1>
+      <p>
+        Our application is designed to provide users with a seamless experience
+        for accessing and managing information. Whether you're a new user
+        exploring our features or a returning visitor, we strive to make your
+        experience enjoyable and productive.
+      </p>
+      <p>
+        If you have any feedback, suggestions, or issues regarding our
+        application, please don't hesitate to contact us. Your input is
+        invaluable to us as we work to improve and enhance our services.
+      </p>
+    </div>
+  );
 };
 
 export default Contact;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,171 @@
-const Login = () => {
-    return <h1>Pregled knjiga na ovoj strani</h1>;
-};
+import React, { useState } from 'react';
 
-export default Login;
+function LoginForm() {
+  const [isLoginFormVisible, setLoginFormVisible] = useState(true);
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+  });
+
+  const switchToLoginForm = () => {
+    setLoginFormVisible(true);
+  };
+
+  const switchToRegisterForm = () => {
+    setLoginFormVisible(false);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+
+  const saveFormData = () => {
+    const emptyFields = Object.values(formData).some((value) => value === '');
+    const agreedToTerms = document.querySelector('.check-box').checked;
+  
+    if (emptyFields) {
+      alert('Some field is empty. Please fill in all fields.');
+    } else if (!agreedToTerms) {
+      alert('Please agree to the terms and conditions.');
+    } else {
+      localStorage.setItem('formData', JSON.stringify(formData));
+      alert('Your data has been saved in localStorage!');
+    }
+  };
+  
+  
+
+  const loginUser = () => {
+    
+    const storedFormData = JSON.parse(localStorage.getItem('formData'));
+    if (storedFormData) {
+      if (
+        storedFormData.email === formData.email &&
+        storedFormData.password === formData.password
+      ) {
+        alert('Welcome to the world of books');
+      } else {
+        alert('Incorrect email or password');
+      }
+    } else {
+      alert('No registered users');
+    }
+  };
+
+  return (
+    
+    <section className="login-section" id="login">
+      <div className="paragraph-container">
+       
+        <div className="form-box">
+          <div className="button-box">
+            <div id="btn"></div>
+            <button
+              type="button"
+              className="toggle-btn login"
+              onClick={switchToLoginForm}
+            >
+              Log in
+            </button>
+            <button
+              type="button"
+              className="toggle-btn register"
+              onClick={switchToRegisterForm}
+            >
+              Register
+            </button>
+          </div>
+
+          {isLoginFormVisible ? (
+            <form
+              id="login-form"
+              className="input-group-login"
+              onSubmit={(e) => {
+                e.preventDefault();
+                loginUser();
+              }}
+            >
+              <input
+                type="text"
+                name="email"
+                placeholder="Email Id"
+                value={formData.email}
+                onChange={handleInputChange}
+              />
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter Password"
+                value={formData.password}
+                onChange={handleInputChange}
+              />
+              <button type="submit" className="submit-btn">
+                Log in
+              </button>
+            </form>
+          ) : (
+            <form
+              id="register-form"
+              className="input-group-register"
+              onSubmit={(e) => {
+                e.preventDefault();
+                saveFormData();
+              }}
+            >
+              <input
+                type="text"
+                name="firstName"
+                placeholder="First Name"
+                value={formData.firstName}
+                onChange={handleInputChange}
+              />
+              <input
+                type="text"
+                name="lastName"
+                placeholder="Last Name"
+                value={formData.lastName}
+                onChange={handleInputChange}
+              />
+              <input
+                type="email"
+                name="email"
+                placeholder="Email Id"
+                value={formData.email}
+                onChange={handleInputChange}
+              />
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter Password"
+                value={formData.password}
+                onChange={handleInputChange}
+              />
+              <input
+                type="password"
+                name="confirmPassword"
+                placeholder="Confirm Password"
+                value={formData.confirmPassword}
+                onChange={handleInputChange}
+              />
+              <input type="checkbox" className="check-box" />
+              <span>I agree to the terms and conditions</span>
+              <p></p>
+              <button type="submit" className="submit-btn">
+                Register
+              </button>
+            </form>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default LoginForm;
